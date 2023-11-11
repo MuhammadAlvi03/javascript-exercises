7f9c0413d1bbf8c2a95a037717dd2290eac2d079
Muhammad Alvi <149120789+MuhammadAlvi03@users.noreply.github.com>
2023-11-11 17:55:41 -0600

    complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(text, repeatTimes) {
+    let string = '';
+        if (repeatTimes < 0) {
+            return 'ERROR';
+        } else {
+            for (let i = 0; i < repeatTimes; i++) {
+                string += `${text}`;
+            }
+            return string;
+        }
 };
+
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
