9d4b3b53926832c2b43941753b4c603ee2b40654
Muhammad Alvi <149120789+MuhammadAlvi03@users.noreply.github.com>
2023-11-11 21:10:58 -0600

    pass first 2 tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(givenArray, ...removals) {
+    for (const removal of removals) {
+    const index = givenArray.indexOf(removal);
+    givenArray.splice(index, 1);
+    }
+    return givenArray;
 };
 
 // Do not edit below this line
