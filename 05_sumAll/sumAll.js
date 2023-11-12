a9be01c6d92cadae50291c3b8f8ad8cd6ec22522
Muhammad Alvi <149120789+MuhammadAlvi03@users.noreply.github.com>
2023-11-12 16:51:14 -0600

    complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,16 @@
-const sumAll = function() {
-
-};
+const sumAll = function(x, y) {
+    const higherValue = Math.max(x, y);
+    const lowerValue = Math.min(x, y);
+    let finalSum = 0;
+    if (((x < 0) || (y < 0)) || (typeof(x)) != 'number' || (typeof(y)) != 'number') {
+        return 'ERROR'
+    } else {
+        for (let i = lowerValue; i <= higherValue; i++) { // starts at lower, ends at higher
+            finalSum += i;
+        }
+        return finalSum;
+    }
+}
 
 // Do not edit below this line
 module.exports = sumAll;
