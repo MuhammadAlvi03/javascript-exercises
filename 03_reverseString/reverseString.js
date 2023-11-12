b5fd906e440dad8cc33b3d1d115050152911dd8e
Muhammad Alvi <149120789+MuhammadAlvi03@users.noreply.github.com>
2023-11-11 18:27:59 -0600

    completed exercise 2 and 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    myArray = text.split("");
+    myArrayReversed = myArray.reverse();
+    stringReversed = myArrayReversed.join("");
+    return stringReversed;
 };
 
 // Do not edit below this line
