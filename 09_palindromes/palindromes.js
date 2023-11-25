347fc47fd85a45590e258419bd24e771fbdb56a2
Muhammad Alvi <149120789+MuhammadAlvi03@users.noreply.github.com>
2023-11-25 17:54:18 -0600

    solve 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let strParsed = str.toUpperCase().replace(/[^A-Z0-9]/g, '');
+    let strArr = strParsed.split('');
+    let reverseStrArr = [];
+    for (let i = 0; i < strArr.length; i++) {
+        reverseStrArr.unshift(strArr[i]);
+    }
+    let reverseStr = reverseStrArr.join('');
+    if (reverseStr === strParsed) {
+        return true;
+    } return false;
 };
 
 // Do not edit below this line
