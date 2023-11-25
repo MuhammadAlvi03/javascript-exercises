5bcbdb101b4edb61dd059ab3e5a577a4e4e6e27c
Muhammad Alvi <149120789+MuhammadAlvi03@users.noreply.github.com>
2023-11-25 17:23:44 -0600

    solve 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(numOne, numTwo) {
+	return (numOne + numTwo);
 };
 
-const subtract = function() {
-	
+const subtract = function(numOne, numTwo) {
+	return (numOne - numTwo);
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+  let sum = 0;
+	for (const num of nums) {
+    sum += num;
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  let product = 1;
+  for (const num of nums) {
+    product *= num;
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(numBase, numPower) {
+	return Math.pow(numBase, numPower);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let res = 1;
+	for (let i = num; i > 0; i--) {
+    res *= i;
+  }
+  return res;
 };
 
 // Do not edit below this line
