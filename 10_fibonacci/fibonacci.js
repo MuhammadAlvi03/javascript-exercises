454e1f124aeeae03abab3996b2bb619251ab4b85
Muhammad Alvi <149120789+MuhammadAlvi03@users.noreply.github.com>
2023-11-25 18:10:42 -0600

    solve 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(fibIndex) {
+    if (fibIndex < 0) {
+        return 'OOPS';
+    }
+    let fib = [1, 1];
+    for (let i = 0, j = i+1; j < fibIndex + 1; i++, j++) {
+        let nextFib = (fib[i] + fib[j]);
+        fib.push(nextFib);
+    }
+    let res = fib[fibIndex - 1];
+    return res;
 };
 
 // Do not edit below this line
