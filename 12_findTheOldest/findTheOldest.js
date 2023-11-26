3702e06201d10b0004f8c0d41ff8983918140311
Muhammad Alvi <149120789+MuhammadAlvi03@users.noreply.github.com>
2023-11-25 18:52:58 -0600

    solve 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+// Given an array of objects representing people with a birth and death year, return the oldest person.
 
+const getAge = function(birth, death) {
+    if (!death) {   // if still alive
+        death = new Date().getFullYear();   // sets death to this year
+    }
+    return death - birth;
+};
+
+const findTheOldest = function(people) {
+    let oldestPerson = people.reduce((oldest, currentPerson) => { // accumulator, currentvalue (similar to array[0] and array [1])
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+        return oldestAge < currentAge ? currentPerson : oldest;
+    })
 };
 
 // Do not edit below this line
